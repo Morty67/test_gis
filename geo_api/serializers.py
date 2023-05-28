570adb0e94cae2979e4ea0c372406f15e760c920
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2023-05-28 16:24:02 +0300

    "Updated PlaceSerializer"

--- geo_api/serializers.py
+++ geo_api/serializers.py
@@ -1,8 +1,40 @@
+from django.contrib.gis.geos import GEOSGeometry
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+
 from geo_api.models import Place
 
 
 class PlaceSerializer(serializers.ModelSerializer):
+    geom = serializers.CharField(
+        validators=[
+            UniqueValidator(
+                queryset=Place.objects.all(),
+                message="This coordinate already exists.",
+            )
+        ]
+    )
+
     class Meta:
         model = Place
         fields = ("id", "name", "description", "geom")
+
+    def validate_geom(self, value):
+        try:
+            geom = GEOSGeometry(value)
+        except (ValueError, TypeError) as e:
+            raise serializers.ValidationError("Invalid geometry: " + str(e))
+
+        if geom and not geom.empty and geom.geom_type != "Point":
+            raise serializers.ValidationError("Invalid point geometry.")
+
+        if geom and not geom.empty and (abs(geom.x) > 180 or abs(geom.y) > 90):
+            raise serializers.ValidationError("Invalid coordinate range.")
+
+        return value
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        geom = GEOSGeometry(representation["geom"])
+        representation["geom"] = geom.wkt
+        return representation
