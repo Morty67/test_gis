fcf69df2ed3460ac4dae7ca268695813ea432e8a
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2023-05-28 19:37:54 +0300

    "Added docstrings to serializer"

--- geo_api/serializers.py
+++ geo_api/serializers.py
@@ -6,6 +6,13 @@ from geo_api.models import Place
 
 
 class PlaceSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Place model.
+
+    Validates and serializes the fields of a Place instance.
+
+    """
+
     geom = serializers.CharField(
         validators=[
             UniqueValidator(
@@ -20,6 +27,19 @@ class PlaceSerializer(serializers.ModelSerializer):
         fields = ("id", "name", "description", "geom")
 
     def validate_geom(self, value):
+        """
+        Validates the geom field.
+
+        Args:
+            value (str): The value of the geom field.
+
+        Returns:
+            str: The validated geom value.
+
+        Raises:
+            serializers.ValidationError: If the geom value is invalid.
+
+        """
         try:
             geom = GEOSGeometry(value)
         except (ValueError, TypeError) as e:
@@ -34,6 +54,16 @@ class PlaceSerializer(serializers.ModelSerializer):
         return value
 
     def to_representation(self, instance):
+        """
+        Converts the serialized representation of the instance.
+
+        Args:
+            instance: The Place instance being serialized.
+
+        Returns:
+            dict: The serialized representation of the instance.
+
+        """
         representation = super().to_representation(instance)
         geom = GEOSGeometry(representation["geom"])
         representation["geom"] = geom.wkt
