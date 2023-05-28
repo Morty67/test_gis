91fb96bceeb21c8df73d7891da1e2f7939cc0ee9
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2023-05-28 19:36:02 +0300

    "Added drf_spectacular"

--- test_gis/settings.py
+++ test_gis/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.gis",
+    "drf_spectacular",
     "rest_framework",
     "geo_api",
 ]
@@ -129,3 +130,21 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Geo Gis API",
+    "DESCRIPTION": "API for working with geolocation data",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+    "SWAGGER_UI_SETTINGS": {
+        "deepLinking": True,
+        "defaultModelRendering": "model",
+        "defaultModelsExpandDepth": 2,
+        "defaultModelExpandDepth": 2,
+    },
+}
